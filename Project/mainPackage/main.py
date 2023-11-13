9fc6f893363ebcd15cbc756073ea9077d0d8a772
bcd6746@rit.edu <bcd6746@rit.edu>
2023-11-12 20:10:29 -0500

    added cluster visualiazation

--- Project/mainPackage/main.py
+++ Project/mainPackage/main.py
@@ -1,8 +1,8 @@
-import cv2
-import numpy
-import matplotlib 
+import numpy as np
 import os
 import open3d as o3d
+from matplotlib import pyplot as plt
+
 
 #How do we find the
 
@@ -11,32 +11,50 @@ def main(imgFiles, pointCloudfiles):
         # Check if the file is an image
         if filename.endswith(('.jpg', '.jpeg', '.png', '.gif')):
             print(imgFiles+"\\"+filename)
-            vehicleInfo = findBoundingBox(cv2.imread(imgFiles+"\\"+filename))
-        
+            vehicleInfo = createVehicleInfo((imgFiles+"\\"+filename))
+
+def FillVehics(img, ImageInfo):
+    clusters = ClusterLidar(img)
+    for clusts in clusters:
+        if len(clusts)>10:
+            print(clusts)
+
+
+#create clusters to count and track the cars
+#measure velocity from the movement of one frame to the next
+#compare based on the closest cluster to the position of the cluster in the previous frame
+def ClusterLidar(file):
+    pcd = o3d.io.read_point_cloud(file)
+    points = np.asarray(pcd.points)
+    clustering = o3d.DBSCAN(eps=.5, min_samples=10).fit(points)
+    cluster_labels = clustering.labels_
+    visualize_clusters(points,cluster_labels)
+    return cluster_labels
 
 def createVehicleInfo(file):
     imgeInfo = ImageInfo()
-    fillVehics = fillVehics(file, ImageInfo)
+    fillVehics = FillVehics(file, ImageInfo)
+
     #call functions to set each element of the vehic class
-   
 
-def fillVehics(img,vehic):
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    greyimg1 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+def visualize_clusters(points, cluster_labels):
+    # Visualize clusters
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection='3d')
 
-    #create sift object
-    sift = cv2.SIFT_create()  
+    unique_labels = set(cluster_labels)
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
 
-    key_points, descriptors = sift.detectAndCompute(greyimg1, None)     
+    for cluster, color in zip(unique_labels, colors):
+        cluster_points = points[cluster_labels == cluster]
+        ax.scatter(cluster_points[:, 0], cluster_points[:, 1], cluster_points[:, 2], c=[color], marker='o', s=20)
 
-    keypointImage = cv2.drawKeypoints(img,key_points,None)   
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
 
-    cv2.imshow("show",keypointImage)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows
 
-
-  
 if __name__=="__main__": 
     #LOAD Folders 
     imgFiles = "F:\\computer vision final project\\traffic-light\\dataset\\Images" 
